fb73eacf9d3758098c4e6fed32069ee9ab6d8ae8
aragakerubo <aragakerubo@gmail.com>
2024-08-08 18:44:06 -0400

    2. Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,12 @@
+-- Write a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
+
+-- Requirements:
+
+-- Import this table dump: metal_bands.sql.zip
+-- Column names must be: origin and nb_fans
+-- Your script can be executed on any database
+-- Context: Calculate/compute something is always power intensive… better to distribute the load!
+
 SELECT origin, COUNT(*) AS nb_fans
 FROM bands
 JOIN fans ON bands.id = fans.band_id
