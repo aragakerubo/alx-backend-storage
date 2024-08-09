f48f6fe706ce3c862fba2195f0e291806cc710c0
aragakerubo <aragakerubo@gmail.com>
2024-08-08 20:56:46 -0400

    5. Email validation to sent

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -8,7 +8,8 @@ CREATE TRIGGER valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-  IF NEW.email NOT REGEXP '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$' THEN
+  IF NEW.email NOT REGEXP '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+  OR NEW.email <> OLD.email THEN
     SET NEW.valid_email = 0;
   END IF;
 END;
