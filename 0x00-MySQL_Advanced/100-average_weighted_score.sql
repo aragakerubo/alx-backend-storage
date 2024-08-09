ba9d5bf88bf38cfc43c56c4ca11a257b38ad106b
aragakerubo <aragakerubo@gmail.com>
2024-08-08 21:39:29 -0400

    12. Average weighted score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,3 +1,17 @@
+-- Write a SQL script that creates a stored
+-- procedure ComputeAverageWeightedScoreForUser that computes
+-- and store the average weighted score for a student.
+
+-- Requirements:
+
+-- Procedure ComputeAverageScoreForUser is taking 1 input:
+-- user_id, a users.id value
+-- (you can assume user_id is linked to an existing users)
+
+-- Tips:
+
+-- Calculate-Weighted-Average
+
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
