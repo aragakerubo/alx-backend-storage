496f4fcbb512cfc2e4e1e38acce7fe63ca100707
aragakerubo <aragakerubo@gmail.com>
2024-08-08 20:31:22 -0400

    3. Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,13 @@
-SELECT band_name,
+-- Write a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+
+-- Requirements:
+
+-- Import this table dump: metal_bands.sql.zip
+-- Column names must be: band_name and lifespan (in years until 2022 - please use 2022 instead of YEAR(CURDATE()))
+-- You should use attributes formed and split for computing the lifespan
+-- Your script can be executed on any database
+
+SELECT band_name, 
 ABS(SUBSTRING_INDEX(lifespan, ' - ', 1) - SUBSTRING_INDEX(lifespan, ' - ', -1))
 AS lifespan
 FROM bands
