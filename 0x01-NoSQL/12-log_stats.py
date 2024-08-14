9a9705dd4a3189b25d7a16909b937d0bccdc3511
aragakerubo <aragakerubo@gmail.com>
2024-08-13 21:18:02 -0400

    12. Log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" 12. Log stats """
+from pymongo import MongoClient
+
+
+def log_stats(mongo_collection):
+    """log stats"""
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(f"{mongo_collection.estimated_document_count()} logs")
+    print("Methods:")
+    for m in method:
+        print(
+            f"\tmethod {m}: {mongo_collection.count_documents({'method': m})}"
+        )
+    print(
+        f"{mongo_collection.count_documents(
+            { 'method': 'GET', 'path': '/status' }
+            )} status check"
+    )
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    log_stats(client.logs.nginx)
