e5a5c7c6711048ca3e66f7ba8fb6dd063930c5f5
aragakerubo <aragakerubo@gmail.com>
2024-08-15 22:28:47 -0400

    5. Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -17,8 +17,12 @@ def count_requests(method: Callable) -> Callable:
     def wrapper(*args, **kwargs):
         key = f"count:{args[0]}"
         r.incr(key)
-        r.expire(key, 10)
-        return method(*args, **kwargs)
+        cached_response = r.get(args[0])
+        if cached_response:
+            return cached_response.decode("utf-8")
+        response = method(*args, **kwargs)
+        r.setex(args[0], 10, response)
+        return response
 
     return wrapper
 
