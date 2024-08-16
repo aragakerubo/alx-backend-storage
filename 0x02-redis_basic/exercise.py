d78589fe30ac7d4b42379c47572e38f54119c766
aragakerubo <aragakerubo@gmail.com>
2024-08-15 20:39:29 -0400

    1. Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
-"""0. Writing strings to Redis"""
+"""
+1. Reading from Redis and recovering original type
+"""
+
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
     """Cache class"""
 
+    # 0. Writing strings to Redis
     def __init__(self):
         """Constructor"""
         self._redis = redis.Redis()
@@ -19,3 +23,23 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    # 1. Reading from Redis and recovering original type
+    def get(
+        self, key: str, fn: Callable = None
+    ) -> Union[str, bytes, int, float]:
+        """Get data from Redis"""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Get a string from Redis"""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """Get an integer from Redis"""
+        return self.get(key, fn=lambda d: int(d))
